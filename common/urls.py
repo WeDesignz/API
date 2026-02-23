781fab5fd388989f876842cf8dbcf1e7df1203e2
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2026-02-23 20:21:56 +0530

    added pinterest post apis in adminpanel

--- common/urls.py
+++ common/urls.py
@@ -19,7 +19,12 @@ urlpatterns = [
     path('update-board', views.pinterest_update_board, name='pinterest_update_board_no_slash'),
     path('delete-board/<str:board_id>/', views.pinterest_delete_board, name='pinterest_delete_board'),
     path('delete-board/<str:board_id>', views.pinterest_delete_board, name='pinterest_delete_board_no_slash'),
-    
+    path('posts/', views.pinterest_posts_list, name='pinterest_posts_list'),
+    path('posts', views.pinterest_posts_list, name='pinterest_posts_list_no_slash'),
+    path('posts/bulk-post/', views.pinterest_posts_bulk_post, name='pinterest_posts_bulk_post'),
+    path('posts/bulk-post', views.pinterest_posts_bulk_post, name='pinterest_posts_bulk_post_no_slash'),
+    path('posts/<int:post_id>/retry/', views.pinterest_post_retry, name='pinterest_post_retry'),
+    path('posts/<int:post_id>/retry', views.pinterest_post_retry, name='pinterest_post_retry_no_slash'),
     # Instagram endpoints
     path('instagram/authorize/', views.instagram_oauth_initiate, name='instagram_oauth_initiate'),
     path('instagram/authorize', views.instagram_oauth_initiate, name='instagram_oauth_initiate_no_slash'),
